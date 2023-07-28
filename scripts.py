31597368241264b3ceeea34939e4f92cba472439
daniel <daniel.enrriquesmercedes@gmail.com>
2023-07-28 12:22:02 +0200

    ft: f-string is the same a string template from js

--- scripts.py
+++ scripts.py
@@ -3,8 +3,10 @@
 name = input("what's your name?")
 
 # Say hello to user
-print("Hello", name)
-
+print("Hello," + name, end=' --> ')
+print('YOU BITCH')
+print(f"hello, {name}");
+# str - string.
 
 
 
